ae438f4fe9c4356e949e3c9615c156d686ab5432
Andrew Peterson <andrewpeterson86@gmail.com>
2013-07-21 16:06:05 +0000

    Modified grader to accept a URL to locate the html file to check.

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -45,14 +46,33 @@ var loadChecks = function(checksfile) {
 };
 
 var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+    return checkHtml(cheerioHtmlFile(htmlfile), checksfile);
+};
+
+var checkUrl = function(url, checksfile) {
+    rest.get(url).on('complete', function(result) {
+	if (result instanceof Error) {
+	    sys.puts('Error: ' + result.message);
+	} else {
+	    var checkJson = checkHtml(cheerio.load(result), checksfile);
+	    printJsonResults(checkJson);
+	}
+    });
+};
+
+var checkHtml = function(htmldata, checksfile) {
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
+        var present = htmldata(checks[ii]).length > 0;
         out[checks[ii]] = present;
     }
     return out;
+};
+
+var printJsonResults = function(checkJson) {
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
 };
 
 var clone = function(fn) {
@@ -65,10 +85,20 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_to_html>', 'Base URL to site index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    var checkJson = null;
+    if (program.url) {
+	checkUrl(program.url, program.checks);
+	// checkUrl is asynchronous so output of the results
+	// will be performed by the callbacks.
+    }
+    else {
+	checkJson = checkHtmlFile(program.file, program.checks);
+	printJsonResults(checkJson);
+    }
+
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
